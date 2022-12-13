4d875ea85b52150517093b4e6a835067772e9f93
Bhavya Bhut <pbhavy4@gmail.com>
2022-12-13 18:36:45 +0530

    update : structure

---
+++ components/index.tsx
@@ -0,0 +1,18 @@
+import Navbar from './Navbar/Navbar';
+import { TypingText, TitleText } from './CustomTexts/CustomTexts';
+import ExploreCard from './ExploreCard/ExploreCard';
+import StartSteps from './StartSteps/StartSteps';
+import NewFeatures from './NewFeatures/NewFeatures';
+import InsightCard from './InsigtCard/InsightCard';
+import Footer from './Footer/Footer';
+
+export {
+  Navbar,
+  TitleText,
+  TypingText,
+  StartSteps,
+  NewFeatures,
+  ExploreCard,
+  InsightCard,
+  Footer,
+};
